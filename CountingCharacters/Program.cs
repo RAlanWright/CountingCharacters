e3730f7fa95fc85959c11dfcf07a7f25e899cc6e
RAlanWright <wright.alan88@gmail.com>
2019-06-05 16:05:35 -0500

    Hardcoded string for character counting.  Working with Collections.Generic

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -10,6 +10,32 @@ namespace CountingCharacters
     {
         static void Main(string[] args)
         {
+            string testString = "This is a test string" +
+                " that holds several characters for " +
+                "matching characters and number of times " +
+                "the character appears.";
+
+            Dictionary<char, int> charCount = new Dictionary<char,int>();
+            char[] charactersInString = testString.ToCharArray();
+
+            foreach (char character in charactersInString) {
+                if (charCount.ContainsKey(character))
+                {
+                    charCount[character] = charCount.Count + 1;
+                } else
+                {
+                    charCount[character] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> singleChar in charCount)
+                {
+                    Console.WriteLine(singleChar.Key + ": " + singleChar.Value);
+                }
+
+
+
+            Console.ReadLine();
         }
     }
 }
